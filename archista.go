3c505c9ede862509e107b7534db27e35c113772f
KarolosLykos <l.g.karolos@gmail.com>
2022-09-20 17:14:06 +0300

    add: docker module

--- archista.go
+++ archista.go
@@ -10,6 +10,7 @@ import (
 	"github.com/KarolosLykos/archista/config"
 	"github.com/KarolosLykos/archista/cpu"
 	"github.com/KarolosLykos/archista/date"
+	"github.com/KarolosLykos/archista/docker"
 	"github.com/KarolosLykos/archista/lights"
 	"github.com/KarolosLykos/archista/logo"
 	medias "github.com/KarolosLykos/archista/media"
@@ -43,6 +44,7 @@ func main() {
 	lightsCM := lights.GetLights(cfg)
 	arch := logo.GetLogo()
 	update := updates.GetUpdates()
+	dockerModule := docker.New()
 
-	panic(barista.Run(update, lightsCM, source, mediaMM, temp, localDate, localTime, arch))
+	panic(barista.Run(dockerModule, update, lightsCM, source, mediaMM, temp, localDate, localTime, arch))
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	barista.run v0.0.0-20211214185156-e6bbbf9b399c
 	github.com/amimof/huego v1.2.1
 	github.com/martinlindhe/unit v0.0.0-20190604142932-3b6be53d49af
-	github.com/spf13/afero v1.9.2 // indirect
+	github.com/spf13/afero v1.9.2
 	github.com/spf13/viper v1.13.0
 	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e
